4796cb694860ef86a71f039f7a38287534bad954
Vitor Rezende <vitorrezende@dcc.ufmg.br>
2022-02-02 21:35:08 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 4

--- lists/views.py
+++ lists/views.py
@@ -8,7 +8,11 @@ def home_page(request):
         return redirect('/lists/the-only-list-in-the-world/')
 
     return render(request, 'home.html')
-    
+
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
